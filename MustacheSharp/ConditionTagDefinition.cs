0da3740aa0acf2ed0781b95d7fdd1e5de6d3d4e6
Michael <mi@art-ist.cc>
2025-05-23 10:37:34 +0200

    WIP: fix build for test

--- MustacheSharp/ConditionTagDefinition.cs
+++ MustacheSharp/ConditionTagDefinition.cs
@@ -2,7 +2,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.Serialization.Configuration;
 using Newtonsoft.Json.Linq;
 
 namespace Mustache
